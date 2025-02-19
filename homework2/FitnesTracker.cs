68588fb9b820c925f264499043f2f75c1b8f02e1
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-19 17:54:12 +0400

    Create FitnesTracker.cs

---
+++ homework2/FitnesTracker.cs
@@ -0,0 +1,25 @@
+namespace FitnesTracker
+{
+    public class WorkOutSession{
+        public string exerciseType;
+        public int durationInMinutes;
+
+        public WorkOutSession(string e, int s){
+            exerciseType = e;
+            durationInMinutes = d;
+        }
+        public void ShowWorkoutDetails(){
+            Console.WriteLine($"The exercise type is {exerciseType}, duration in minutes is {durationInMinutes}");
+        }   
+    }
+
+    internal class BookLibrary{
+        static void Main(string[] args){
+            WorkOutSession W1 = new WorkOutSession("mi ban", 30);
+            WorkOutSession W2 = new WorkOutSession("Mi ban", 20);
+
+            W1.ShowWorkoutDetails;
+            W2.ShowWorkoutDetails;
+        }
+    }
+}
