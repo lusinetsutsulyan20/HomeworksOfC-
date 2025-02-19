80719612e74ac7e7e4e888e494ea999ad83ea1e3
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-19 17:46:00 +0400

    Create StepCounter.cs

---
+++ homework2/StepCounter.cs
@@ -0,0 +1,27 @@
+namespace StepCounter
+{
+    public class Smartwatch{
+        public string ownerName;
+        public int stepCount;
+
+        public Smartwatch(string name){
+            ownerName = name;
+            stepCount = 0;
+        }
+        public void AddSteps(int steps){
+            stepCount += steps;
+        }
+        public void ShowSteps(){
+            Console.WriteLine($"The {name}'s steps count is {stepCount}");
+        }
+    }
+
+     internal class FlightReservation{
+        static void Main(string[] args){
+            Smartwatch S = new Smartwatch("anun");
+            S.AddSteps(3);
+            S.ShowSteps();
+
+        }
+     }
+}
