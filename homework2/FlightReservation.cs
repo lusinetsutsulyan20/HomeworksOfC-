f02dd1c96f6523a3627cf229bcd42ad682123eff
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-19 17:21:17 +0400

    Create FlightReservation.cs

---
+++ homework2/FlightReservation.cs
@@ -0,0 +1,24 @@
+namespace FlightReservation
+{
+    public class FlightTicket{
+        public string passengerName;
+        public int flightNumber;
+        public int seatNumber;
+        
+        public FlightTicket(string name; int fNumber; int sNumber){
+            passengerName = name;
+            flightNumber = fNumber;
+            seatNumber = sNumber;
+        }
+    }
+
+    internal class FlightReservation{
+        static void Main(string[] args){
+            FlightTicket flight = new FlightTicket("anun", 5, 1);
+            Console.WriteLine(flight.passengerName);
+            Console.WriteLine(flight.flightNumber);
+            Console.WriteLine(flight.seatNumber);
+        }
+    }
+
+}
