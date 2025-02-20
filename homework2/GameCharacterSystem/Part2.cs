ecc610a917aeb2fa0ab3a162b9cfc2101be3bcb5
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-20 18:21:38 +0400

    Create Part2.cs

---
+++ homework2/GameCharacterSystem/Part2.cs
@@ -0,0 +1,8 @@
+namespace Part2
+{
+    public partial class GameClass{
+        public void ShowCharacterInfo(){
+            Console.WriteLine($"The name is {characterName}, level is {level}");
+        }
+    }
+}
