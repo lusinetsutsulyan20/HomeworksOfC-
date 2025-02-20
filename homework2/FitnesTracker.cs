6ae243101fab47d27655924256973a47c1e631a1
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-20 18:47:58 +0400

    Update FitnesTracker.cs

--- homework2/FitnesTracker.cs
+++ homework2/FitnesTracker.cs
@@ -6,7 +6,7 @@ namespace FitnesTracker
 
         public WorkOutSession(string e, int s){
             exerciseType = e;
-            durationInMinutes = d;
+            durationInMinutes = s;
         }
         public void ShowWorkoutDetails(){
             Console.WriteLine($"The exercise type is {exerciseType}, duration in minutes is {durationInMinutes}");
@@ -18,8 +18,8 @@ namespace FitnesTracker
             WorkOutSession W1 = new WorkOutSession("mi ban", 30);
             WorkOutSession W2 = new WorkOutSession("Mi ban", 20);
 
-            W1.ShowWorkoutDetails;
-            W2.ShowWorkoutDetails;
+            W1.ShowWorkoutDetails();
+            W2.ShowWorkoutDetails();
         }
     }
 }
