796b6827718e645e704a686e6677bc50b20e5fbc
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-23 16:55:57 +0400

    Create MaximumNumber.cs

---
+++ Homework3/MaximumNumber.cs
@@ -0,0 +1,23 @@
+using System;
+public class MaxNumber{
+    public static void MaxOfArray(ref int n, params int[] numbers){
+        int max = n;
+        foreach (int num in numbers){
+            if (num > max){
+                max = num;
+            }
+        }
+        Console.WriteLine($"The maximum number of array is {max}");
+    }
+}
+ internal class BookLibrary{
+    static void Main(string[] args){
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.Length; i++){
+            arr[i] = int.Parse(Console.ReadLine() ?? "");
+        }
+        int max = 5;
+        Program.MaxOfArray(ref max, arr);
+        Console.WriteLine(max);
+   }
+}
