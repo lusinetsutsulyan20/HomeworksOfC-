b61b5c4dcdd18609fb6a4ee704a1a73999b390fc
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-02-23 16:24:25 +0400

    Update BasicCalculator.cs

--- Homework3/BasicCalculator.cs
+++ Homework3/BasicCalculator.cs
@@ -1,5 +1,6 @@
+
 using System;
-class BasicCalculator {
+public static class BasicCalculator {
     public static int Add (int a, int b){
         return a + b;
     }
@@ -16,30 +17,156 @@ class BasicCalculator {
         return (a / b);
     }
        
-       
-    static void Main() {
-        int a = int.Parse(Console.ReadLine() ?? "");
-        int b = int.Parse(Console.ReadLine() ?? "");
+    public static void Calculate(){
+        Console.WriteLine("Enter action");
+        string i = Console.ReadLine() ?? "";
+        if (i == "x"){
+            Console.WriteLine("Recursion finished");
+            return;
+        }
+        Console.WriteLine ("Enter two numbers");
+        int first = int.Parse(Console.ReadLine() ?? "");
+        int second = int.Parse(Console.ReadLine() ?? "");
         
-        int res = Add (a, b);
-        Console.WriteLine ($"{a} + {b} = {res}");
-        
-        res = Subtract (a, b);
-        Console.WriteLine ($"{a} - {b} = {res}");
-        
-        res = Multiply (a, b);
-        Console.WriteLine ($"{a} * {b} = {res}");
-        
-        res = Divide (a, b);
-        Console.WriteLine ($"{a} / {b} = {res}");
-        
+        switch(i){
+            case "+":
+                Console.WriteLine(Add(first, second));
+                break;
+            case "-":
+                Console.WriteLine(Subtract(first, second));
+                break;
+            case "*":
+                Console.WriteLine(Multiply(first, second));
+                break;
+            case "/":
+                Console.WriteLine(Divide(first, second));
+                break;
+            default:
+                Console.WriteLine("Enter 'x' for break");
+                break;
+        }
+        Calculate();
     }
+}
+  internal class BookLibrary{
+   static void Main(string[] args){
+    
+       BasicCalculator.Calculate();
+   }
+    
 }
 
 
+using System;
+public static class BasicCalculator {
+    public static int Add (int a, int b){
+        return a + b;
+    }
+    
+    public static int Subtract (int a, int b){
+        return a - b;    
+    }
+       
+    public static int Multiply (int a, int b){
+        return a * b;
+    }
+    
+    public static int Divide (int a, int b){
+        return (a / b);
+    }
+       
+    public static void Calculate(){
+        Console.WriteLine("Enter action");
+        string i = Console.ReadLine() ?? "";
+        if (i == "x"){
+            Console.WriteLine("Recursion finished");
+            return;
+        }
+        Console.WriteLine ("Enter two numbers");
+        int first = int.Parse(Console.ReadLine() ?? "");
+        int second = int.Parse(Console.ReadLine() ?? "");
+        
+        switch(i){
+            case "+":
+                Console.WriteLine(Add(first, second));
+                break;
+            case "-":
+                Console.WriteLine(Subtract(first, second));
+                break;
+            case "*":
+                Console.WriteLine(Multiply(first, second));
+                break;
+            case "/":
+                Console.WriteLine(Divide(first, second));
+                break;
+            default:
+                Console.WriteLine("Enter 'x' for break");
+                break;
+        }
+        Calculate();
+    }
+}
+  internal class BookLibrary{
+   static void Main(string[] args){
+    
+       BasicCalculator.Calculate();
+   }
+    
+}
 
 
-
-
-
-
+using System;
+public static class BasicCalculator {
+    public static int Add (int a, int b){
+        return a + b;
+    }
+    
+    public static int Subtract (int a, int b){
+        return a - b;    
+    }
+       
+    public static int Multiply (int a, int b){
+        return a * b;
+    }
+    
+    public static int Divide (int a, int b){
+        return (a / b);
+    }
+       
+    public static void Calculate(){
+        Console.WriteLine("Enter action");
+        string i = Console.ReadLine() ?? "";
+        if (i == "x"){
+            Console.WriteLine("Recursion finished");
+            return;
+        }
+        Console.WriteLine ("Enter two numbers");
+        int first = int.Parse(Console.ReadLine() ?? "");
+        int second = int.Parse(Console.ReadLine() ?? "");
+        
+        switch(i){
+            case "+":
+                Console.WriteLine(Add(first, second));
+                break;
+            case "-":
+                Console.WriteLine(Subtract(first, second));
+                break;
+            case "*":
+                Console.WriteLine(Multiply(first, second));
+                break;
+            case "/":
+                Console.WriteLine(Divide(first, second));
+                break;
+            default:
+                Console.WriteLine("Enter 'x' for break");
+                break;
+        }
+        Calculate();
+    }
+}
+  internal class BookLibrary{
+   static void Main(string[] args){
+    
+       BasicCalculator.Calculate();
+   }   
+}
