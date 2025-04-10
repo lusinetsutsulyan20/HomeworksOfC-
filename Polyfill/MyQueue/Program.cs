008f54452fedfad579759f55b158b2cc35e9cd97
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-04-11 01:12:26 +0400

    Add files via upload

---
+++ Polyfill/MyQueue/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using MyQueue;
+
+public class Program
+{
+    public static void Main()
+    {
+         Queue<int> numbers = new Queue<int>();
+
+        // adds 65 and 17 to the queue
+        numbers.Enqueue(65);
+        numbers.Enqueue(17);
+
+        // print elements of the queue 
+        foreach (int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
