56c7723ed82b3f3be41f368ccfd688af323ca6aa
lusinetsutsulyan20 <lusinetsutsulyan20@gmail.com>
2025-04-16 18:47:36 +0400

    Update Program.cs

--- Polyfill/MyStack/Program.cs
+++ Polyfill/MyStack/Program.cs
@@ -1,20 +1,20 @@
-﻿using System;
+using System;
 using MyStack;
+using People;
+namespace Prog{
 public class Program
 {
     public static void Main()
     {
-        MyStack<string> arr = new MyStack<string>();
-        arr.Push("a");
-        Console.WriteLine (arr.Peek());
-
-        arr.Push("b");
-        Console.WriteLine (arr.Peek());
-
-        arr.Push("c");
-        Console.WriteLine (arr.Peek());
-
+        MyStack<Person> arr = new MyStack<Person>();
+        arr.Push(new Person("aa", 11));
+        arr.Push(new Person("bb", 22));
+        arr.Push(new Person("cc", 33));
+        arr.Print();
         arr.Pop();
+        Console.WriteLine();
+        arr.Print();
         Console.WriteLine (arr.Peek());
     }
+}
 }
